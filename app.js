910288df8f9db549eafb3c02a5de65901fded20b
EZPlanningStudio <daniellanechita@gmail.com>
2026-09-11 14:08:25 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -4717,6 +4717,10 @@ async function importLatestBackup() {
             return;
         }
 
+        if (imported._activated) {
+            localStorage.setItem("billTrackerActivated", "true");
+        }
+
         data = normalizeAppData(imported);
 
         saveData();
@@ -4782,7 +4786,9 @@ async function autoSaveToBackup() {
 
 async function exportJson() {
     const fileName = "bill-tracker-v1-backup.json";
-    const json = JSON.stringify(data, null, 2);
+    const activated = localStorage.getItem("billTrackerActivated");
+    const exportData = activated ? { ...data, _activated: true } : data;
+    const json = JSON.stringify(exportData, null, 2);
     const blob = new Blob([json], { type: "application/json" });
 
     if (!window.showDirectoryPicker) {
